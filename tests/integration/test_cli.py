fa31ace926df5a4d06f049da60b51329812c83af
jxltom <jxltom@gmail.com>
2018-12-05 10:30:28 +0800

    Add test for pipenv --clear

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -218,3 +218,11 @@ import records
 
         c = p.pipenv('check --unused .')
         assert 'tablib' not in c.out
+
+
+@pytest.mark.cli
+def test_pipenv_clear(PipenvInstance):
+    with PipenvInstance() as p:
+        c = p.pipenv('--clear')
+        assert c.return_code == 0
+        assert 'Clearing caches…' in c.out
